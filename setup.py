f293d8f0b2e980670dbe95238b0c8e4eacd2fadb
Er.Nischal <nishchalmandal@gmail.com>
2025-01-12 00:15:07 +0000

    update to setup

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="multimodal_preprocessing",
-    version="0.1.0",
-    description="A library for multimodal data preprocessing",
+    name="multimodal_preprocessing",  # Unique package name
+    version="0.1.0",  # Initial version
+    description="A library for preprocessing multimodal data (text, numeric, video).",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Your Name",
     author_email="your_email@example.com",
+    url="https://github.com/your-username/multimodal-preprocessing",  # GitHub repo URL
+    license="MIT",
     packages=find_packages(),
     install_requires=[
         "numpy",
@@ -15,4 +19,9 @@ setup(
         "imbalanced-learn",
     ],
     python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
